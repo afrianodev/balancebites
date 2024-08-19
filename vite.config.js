d100ffa07b8a156437f71c95c7a7b38c60903ec9
piperiao <andresfeliperianom@gmail.com>
2024-08-18 21:40:42 -0500

    update url to my portfolio

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    host: true,  // Allow access from any IP address
+    port: 5173,       // Default port (can be changed if needed)
+    strictPort: true, // Ensure Vite uses this port exactly
+  },
 });
